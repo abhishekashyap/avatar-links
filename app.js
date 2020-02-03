cd5ac235d5ae1b74d4c5b44290d9223345fce59b
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-02-03 23:35:37 +0530

    Created random index function

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+'use strict';
+
 const Koa = require('koa');
 const Router = require('koa-router');
+const serve = require('koa-static');
+const path = require('path');
+const glob = require('glob');
 
 const app = new Koa();
 const router = new Router();
@@ -8,10 +13,36 @@ app.use(router.allowedMethods());
 app.use(router.routes());
 app.use(require('koa-body')()); // Body parser
 
+app.use(serve(path.join(__dirname, 'avatars'))); // Serving a static folder
 
-app.use(ctx => {
-    ctx.body = 'Hello';
-})
+router.get('/svg', ctx => {
+    // random svg
+    let filenames = glob.sync('avatars/*.svg').map(name => path.basename(name));
+    let n = Math.floor(Math.random() * (filenames.length)); // Generating random array index
+    console.log(filenames.length);
+    console.log(n);
+
+});
+
+router.get('/boy-svg', ctx => {
+
+});
+
+router.get('/girl-svg', ctx => {
+
+});
+
+router.get('/png', ctx => {
+
+});
+
+router.get('/boy-png', ctx => {
+
+});
+
+router.get('/girl-png', ctx => {
+
+});
 
 const PORT = 3000;
 
