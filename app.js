d5035ff14d7916c8d4ce5d8fe00f520f230d2e95
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-02-04 11:41:46 +0530

    GET requests for format working

--- app.js
+++ app.js
@@ -15,40 +15,25 @@ app.use(require('koa-body')()); // Body parser
 
 app.use(serve(path.join(__dirname, 'avatars'))); // Serving a static folder
 
-function randomFile(format, ctx) {
+async function randomFile(format, ctx) {
     // random svg
-    let filenames = glob.sync(path.join('avatars/*.', format)).map(name => path.basename(name)); // returns basename instead of absolute path
+    
+    let filenames = await glob.sync('avatars/*.' + format).map(name => path.basename(name)); // returns basename instead of absolute path
+    
     let n = Math.floor(Math.random() * (filenames.length)); // Generating random array index
-    return path.join(ctx.host, filenames[n]);
+    
+    return ctx.host + '/' + filenames[n];
 }
 
-// router.get('/:format', cts => {
-
-// });
-
-
-router.get('/svg', ctx => {
-    console.log(randomFile('svg', ctx));
+router.get('/:format', ctx => {
+    randomFile(ctx.params.format, ctx).then((link) => {
+        console.log(link);
+    })
 });
 
-// router.get('/boy-svg', ctx => {
 
-// });
-
-// router.get('/girl-svg', ctx => {
-
-// });
-
-// router.get('/png', ctx => {
-
-// });
-
-// router.get('/boy-png', ctx => {
-
-// });
-
-// router.get('/girl-png', ctx => {
-
+// router.get('/svg', ctx => {
+    
 // });
 
 const PORT = 3000;
